91c2b51cc6ce1ca9baab6ec391b3128e734c8e0c
danajackson2 <danajackson2@gmail.com>
2021-01-19 16:03:10 -0800

    yeehaw

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+function mapToNegativize(sourceArray){
+    return sourceArray.map(item => item * -1)
+}
+
+function mapToNoChange(sourceArray){
+    return sourceArray
+}
+
+function mapToDouble(sourceArray){
+    return sourceArray.map(item => item * 2)
+}
+
+function mapToSquare(sourceArray){
+    return sourceArray.map(item => item**2)
+}
+
+function reduceToTotal(sourceArray, startingPoint=0){
+    let memo = startingPoint
+    sourceArray.forEach(item => memo += item)
+    return memo
+}
+
+function reduceToAllTrue(sourceArray){
+    let memo = true
+    sourceArray.forEach(item => {
+        if (!item) {memo = false}
+    })
+    return memo 
+}
+
+function reduceToAnyTrue(sourceArray){
+    let memo = false
+    sourceArray.forEach(item => {
+        if (item) {memo = true}
+    })
+    return memo 
+}
+
